a56b2b158d4215ec6f9368de27a94aa822aecaef
在红尘中成仙 <jiazhen6@illinois.edu>
2022-04-08 23:09:35 -0500

    fix some flaky tests

--- src/test/java/pair/distribution/app/helpers/DayPairsHelperTest.java
+++ src/test/java/pair/distribution/app/helpers/DayPairsHelperTest.java
@@ -210,7 +210,7 @@ public class DayPairsHelperTest {
 		DayPairs dayPairs = subject.generateNewDayPairs(tracks, devs, pairs, pairsWeight, getStandardCompanies());
 
 		assertThat(dayPairs.getTracks().size(), is(2));
-	  assertThat(dayPairs.getTracks(), containsInAnyOrder("track1", "track2"));
+	  	assertThat(dayPairs.getTracks(), containsInAnyOrder("track1", "track2"));
 		assertThat(dayPairs.hasPair(new Pair(Arrays.asList(new Developer("dev1"), new Developer("dev4")))), is(true));
 		assertThat(dayPairs.hasPair(new Pair(Arrays.asList(new Developer("dev3")))), is(true));
 		boolean trackOneHasContext = dayPairs.getPairByTrack("track1").getFirstDev().hasContext() || dayPairs.getPairByTrack("track1").getSecondDev().hasContext();
